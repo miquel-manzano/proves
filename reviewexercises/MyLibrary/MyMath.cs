0409b0c2a138c8ba31457cb7013d89c84c7a1325
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-01-08 13:05:50 +0100

    Almost Finish..

---
+++ reviewexercises/MyLibrary/MyMath.cs
@@ -0,0 +1,10 @@
+﻿namespace MyLibrary
+{
+    public static class MyMath
+    {
+        public static float TriangleArea(float _base, float height)
+        {
+            return (_base * height) / 2;
+        }
+    }
+}
---
+++ reviewexercises/MyLibrary/UserInteraction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public static class UserInteraction
+    {
+        public static float UserInputFloat()
+        {
+            float userNum;
+            try
+            {
+                userNum = float.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                userNum = 0.0f;
+            }
+            return userNum;
+        }
+    }
+}
